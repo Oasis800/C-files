8c1f1a9eaafbd34c428682b0ad53fdea843a17f7
oasis636 <125544598+oasis636@users.noreply.github.com>
2024-03-14 22:51:26 +0000

    everything is possible

---
+++ Hello_World/DigitAdd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * A program that prints  adds 
+ * the digits of a number till it's less than 10
+ * Return - Always(0);
+ */
+
+int main()
+{
+    int num[10], sum = 0, count;
+    printf("How many numbers are you going to deal with?\n");
+    scanf("%d", &count);
+    printf("Enter numbers one by one:\n");
+    for(int i = 0; i < count; i++)
+    scanf("%d", &num[i]);
+
+    for(int i = 0; i < count; i++)
+     sum += num[i] ;
+
+    printf("First value for sum: %d\n", sum);
+
+    return 0;
+}
--- Hello_World/README.md
+++ Hello_World/README.md
@@ -1,2 +1,3 @@
 This basis of C programming will include the following topics: variables, datatypes and conditional statements.
 Here, I have used C to write a simple program that prints "Hello World!".
+Also, I have created a program that adds the digits in a number when the number is entered as an array.
